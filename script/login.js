5bb81121bbd52f3808f7704abcd04db4052bacde
Vitor Gabriel dos Santos <36780996+ovitorsantos@users.noreply.github.com>
2018-03-25 21:18:59 -0300

    Projeto
    Projeto ECAMARA

---
+++ script/login.js
@@ -0,0 +1,28 @@
+
+     function onSignIn(googleUser) {
+          var profile = googleUser.getBasicProfile();
+          console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+          console.log('Name: ' + profile.getName());
+          console.log('Image URL: ' + profile.getImageUrl());
+          console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+            
+            $(".g-signin2").css("display","none")
+            $(".data").css("display", "block")
+            $("#name").text(profile.getName())
+            $("#email").text(profile.getEmail())    
+
+}
+
+function onSignOut(){
+        var auth2 = gapi.auth2.getAuthInstance()
+        auth2.signOut().then(function(){
+        alert("Voce saiu da conta")
+
+        $(".g-signin2").css("display","block")
+        $(".data").css("display", "none")
+
+        
+
+})
+}
+
